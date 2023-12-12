a667db23f65ceae2cf074c97a4ad8e1bfc16eb27
Honzer <82900475+Lemonlemonon@users.noreply.github.com>
2023-12-12 16:54:22 +0000

    Update PhotoController.java

--- src/main/java/com/fyp/mumarket/controller/common/PhotoController.java
+++ src/main/java/com/fyp/mumarket/controller/common/PhotoController.java
@@ -1,8 +1,4 @@
 package com.fyp.mumarket.controller.common;
-/**
- * Image viewing controller
- */
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -12,11 +8,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.fyp.mumarket.util.PathUtil;
-
+//Image viewing controller
 @RequestMapping("photo")
 @Controller
 public class PhotoController {
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+    //Universal image viewer method
+    @RequestMapping(value = "/view")
+    @ResponseBody
+    public ResponseEntity<?> viewPhoto(@RequestParam(name = "filename", required = true) String filename) {
+        String uploadPhotoPath = PathUtil.newInstance().getUploadPhotoPath();
+        Resource resource = resourceLoader.getResource("file:" + uploadPhotoPath + "/" + filename);
+        try {
+            return ResponseEntity.ok(resource);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
